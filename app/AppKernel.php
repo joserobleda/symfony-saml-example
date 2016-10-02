4b9be4809e78a924ed3aa8ed2e37263d07dd6360
Jose Ignacio <joserobleda@gmail.com>
2016-10-02 12:59:00 +0200

    symfony google sso saml

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new LightSaml\SymfonyBridgeBundle\LightSamlSymfonyBridgeBundle(),
+            new LightSaml\SpBundle\LightSamlSpBundle(),
             new AppBundle\AppBundle(),
         ];
 
---
+++ src/AppBundle/Security/AdminUserMockProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Security;
+
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\User;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class AdminUserMockProvider implements UserProviderInterface
+{
+    public function loadUserByUsername($username)
+    {
+        $user = new User($username, '', ['ROLE_ADMIN']);
+
+        return $user;
+    }
+
+    public function refreshUser(UserInterface $user)
+    {
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    public function supportsClass($class)
+    {
+        return (bool) $class;
+    }
+}
